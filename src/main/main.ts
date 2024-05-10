cdc5720584dafaae480f128b3752f9e239832488
朱豪杰 <17600515864@163.com>
2024-05-10 21:42:14 +0800

    feat: ✨ BrowserWindow使用单例模式

--- src/main/main.ts
+++ src/main/main.ts
@@ -3,16 +3,16 @@ import path from 'path'
 import { updateElectronApp } from 'update-electron-app'
 import { registerIpcHandlers } from './ipcHandlers'
 
-let devtools: BrowserWindow | undefined
+// let devtools: BrowserWindow | undefined
 
-// Handle creating/removing shortcuts on Windows when installing/uninstalling.
-if (require('electron-squirrel-startup')) {
-  app.quit()
-}
+let mainWindow: BrowserWindow | undefined
 
 const createWindow = () => {
-  // Create the browser window.
-  const mainWindow = new BrowserWindow({
+  if (mainWindow) {
+    return
+  }
+
+  mainWindow = new BrowserWindow({
     width: 800,
     height: 640,
     webPreferences: {
@@ -22,16 +22,15 @@ const createWindow = () => {
     }
   })
 
-  // 开发者工具单独打开一个窗口
-  devtools = new BrowserWindow()
-
   // 加载页面
   if (MAIN_WINDOW_VITE_DEV_SERVER_URL) {
     mainWindow.loadURL(MAIN_WINDOW_VITE_DEV_SERVER_URL)
 
-    // Open the DevTools.
-    mainWindow.webContents.setDevToolsWebContents(devtools.webContents)
-    mainWindow.webContents.openDevTools({ mode: 'detach' })
+    mainWindow.webContents.openDevTools()
+    // 开发时 如果碰到打不开开发者工具就使用下面的代码，单独打开一个窗口
+    // devtools = new BrowserWindow()
+    // mainWindow.webContents.setDevToolsWebContents(devtools.webContents)
+    // mainWindow.webContents.openDevTools({ mode: 'detach' })
   } else {
     mainWindow.loadFile(path.join(__dirname, `../renderer/${MAIN_WINDOW_VITE_NAME}/index.html`))
   }
@@ -39,7 +38,6 @@ const createWindow = () => {
 
 app.whenReady().then(() => {
   createWindow()
-
   Menu.setApplicationMenu(null)
 
   // 设置 Content-Security-Policy（CSP），跨站脚本攻击 (XSS) 和其他代码注入攻击
@@ -53,14 +51,16 @@ app.whenReady().then(() => {
   })
 
   // 更新Electron应用
-  updateElectronApp()
+  if (!MAIN_WINDOW_VITE_DEV_SERVER_URL) {
+    updateElectronApp()
+  }
+})
 
-  app.on('activate', () => {
-    // 在macOS上，当单击dock图标并且没有其他窗口打开时，通常在应用程序中重新创建一个窗口
-    if (BrowserWindow.getAllWindows().length === 0) {
-      createWindow()
-    }
-  })
+// 在macOS上，当单击dock图标并且没有其他窗口打开时，通常在应用程序中重新创建一个窗口
+app.on('activate', () => {
+  if (BrowserWindow.getAllWindows().length === 0) {
+    createWindow()
+  }
 })
 
 // 所有窗口关闭时，且不是macOS时退出应用（通常情况下，macOS应用会保持激活状态，直到用户手动退出）
